26a6991e3e3cae47eaf2eca9a0b350406cfbc00e
NamrataKatkar <51794565+NamrataKatkar@users.noreply.github.com>
2019-07-13 20:20:09 -0400

    adedd code with o(n*2) complexity

--- 3sums.py
+++ 3sums.py
@@ -1,5 +1,7 @@
 *** find 3 elements from the list that have minimum difference with the given target value.***
 
+# solution 1 - complexity o(n*3)
+
 def three_sum_diff(l,target):
     curr_val=0
     prev_val=float("inf")
@@ -13,8 +15,28 @@ def three_sum_diff(l,target):
                     op.append([l[i],l[j],l[k]])
                     prev_val=curr_val
     return op
+
+# solution 2 - complexity o(n*2)
+
+def 3sum(l,target):
+    l.sort()
+    op=[]
+    for i in range(0,len(a)-2):
+        f=i+1
+        l=len(a)-1
+        while f<l:
+            if a[i]+a[f]+a[l]==0:
+                op.append([a[i],a[f],a[l]])
+                f=f+1
+            elif a[i]+a[f]+a[l] > 0:
+                l=l-1
+            else:
+                f=f+1
+     return op
+               
    
 if __name__=="__main__":
     l=[1,2,3,4,5,6,7,8]
     target=int(input("input th number : "))
     print(three_sum_diff(l,target))
+    print(3sum(l,target))
